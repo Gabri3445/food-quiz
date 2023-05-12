42b3926aad6614607ef1e641cc378ab694b1371e
Gabriele Lopez <gabriele.ope@gmail.com>
2023-05-12 19:16:34 +0200

    Dunno

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,60 @@
-import {GetStaticProps, type NextPage} from "next";
+import {GetServerSideProps, type NextPage} from "next";
 import {ChangeEvent, MouseEvent, useState} from "react";
-import Form, {FormProps} from "~/components/Form";
+import Form from "~/components/Form";
 import Question from "~/types/Question";
 import path from "path";
-import Result from "~/types/Result";
 import {promises as fs} from 'fs';
+import {useRouter} from "next/router";
 
 interface Props {
     questions: Question[];
-    results: Result[];
 }
 
 const Home: NextPage<Props> = (props: Props) => {
     let [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
     let [score, setScore] = useState(0);
+    let [checkedElement, setCheckedElement] = useState<HTMLInputElement | null>(null);
+    const router = useRouter();
 
-    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-        console.log(e.target);
+    const handleClick = async (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        if (currentQuestionNumber < props.questions.length - 1 && checkedElement) {
+            setCurrentQuestionNumber(currentQuestionNumber + 1);
+            checkedElement.checked = false;
+            setCheckedElement(null);
+        } else if (checkedElement) {
+            await router.push(`/result/${score}`);
+        }
     };
 
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target);
+    const onFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const target = e.target as HTMLInputElement;
+        setCheckedElement(target);
+        setScore(score + Number(target.value));
     };
-
-    const props2 = {
-        questions: [
-            {
-                question: "What is your name?",
-                answers: [
-                    {
-                        answer: "Joe",
-                        score: 1
-                    },
-                    {
-                        answer: "Jane",
-                        score: 2
-                    }
-                ]
-            }
-        ]
-    };
-
-    const formProps: FormProps = {
-        question: props2.questions[currentQuestionNumber],
-        onClick: handleClick,
-        onChange: onChange
-    };
-
     return (
         <>
-            <Form question={formProps.question} onClick={formProps.onClick} onChange={formProps.onChange}></Form>
+            <Form
+                question={props.questions[currentQuestionNumber]!}
+                onClick={handleClick}
+                onChange={onFormChange}
+            />
         </>
     );
+
+
 };
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
     const directory = path.join(process.cwd(), "data");
     const questions = await fs.readFile(path.join(directory, "questions.json"), "utf-8");
-    const results = await fs.readFile(path.join(directory, "results.json"), "utf-8");
+    const questionsArray = JSON.parse(questions) as Question[];
+    for (const question of questionsArray) {
+        question.answers.sort(() => Math.random() - 0.5);
+    }
     return {
         props: {
-            questions: JSON.parse(questions),
-            results: JSON.parse(results)
+            questions: questionsArray
         }
     };
 };
