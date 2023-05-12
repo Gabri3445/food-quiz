e3efc401b4fb0efb78c83fc2ea9e96c8a7926d20
Gabriele Lopez <gabriele.ope@gmail.com>
2023-05-12 22:42:26 +0200

    Fixed score

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,20 +17,19 @@ const Home: NextPage<Props> = (props: Props) => {
     const router = useRouter();
 
     const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
         if (currentQuestionNumber < props.questions.length - 1 && checkedElement) {
+            setScore(score + Number(checkedElement.value));
             setCurrentQuestionNumber(currentQuestionNumber + 1);
             checkedElement.checked = false;
             setCheckedElement(null);
         } else if (checkedElement) {
-            router.push(`/result/${score}`).then().catch(console.error);
+            router.push(`/result/${score + Number(checkedElement.value)}`).then().catch(console.error);
         }
     };
 
     const onFormChange = (e: ChangeEvent<HTMLInputElement>) => {
         const target = e.target as HTMLInputElement;
         setCheckedElement(target);
-        setScore(score + Number(target.value));
     };
     return (
         <div className="w-screen h-screen  bg-gray-800">
@@ -54,6 +53,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
     const directory = path.join(process.cwd(), "data");
     const questions = await fs.readFile(path.join(directory, "questions.json"), "utf-8");
     const questionsArray = JSON.parse(questions) as Question[];
+    questionsArray.sort(() => Math.random() - 0.5);
     for (const question of questionsArray) {
         question.answers.sort(() => Math.random() - 0.5);
     }
