3378cffc2087bbfe9cf0e26cc9b243a3da40330a
Gabriel Lopez <42178704+Gabri3445@users.noreply.github.com>
2023-05-13 21:30:52 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import path from "path";
 import {promises as fs} from 'fs';
 import Content from "~/components/Content";
 
-export const dynamic = 'force-dynamic';
-
 
 const Page = async () => {
     const fetchData = async (): Promise<Question[]> => {
