73059f2f549644ca834a8959e1388797e69e69c0
Gabriele Lopez <gabriele.ope@gmail.com>
2023-05-13 16:17:48 +0200

    Switched to app router

--- src/app/result/[result]/page.tsx
+++ src/app/result/[result]/page.tsx
@@ -1,21 +1,30 @@
-import type {GetStaticPaths, GetStaticProps, NextPage} from "next";
-import {useRouter} from "next/router";
-import Result from "~/types/Result";
+import Image from "next/image";
 import path from "path";
 import {promises as fs} from "fs";
-import Image from "next/image";
+import Result from "~/types/Result";
 
-interface ResultProps {
-    results: Result[];
+
+interface PageProps {
+    params: {
+        result: number
+    };
 }
 
-const Result: NextPage<ResultProps> = (props: ResultProps) => {
-    const router = useRouter();
-    const score = Number(router.query.result);
+const Page = async ({params}: PageProps) => {
+
+    const fetchData = async (): Promise<Result[]> => {
+        const directory = path.join(process.cwd(), "data");
+        const results = await fs.readFile(path.join(directory, "results.json"), "utf-8");
+        return JSON.parse(results) as Result[];
+    };
+
+    const score = params.result;
+
+    const results = await fetchData();
 
     const determineFood = (): string => {
         if (score) {
-            for (const result of props.results) {
+            for (const result of results) {
                 if (score >= result.scoreRange.minScore && score <= result.scoreRange.maxScore) {
                     return result.result;
                 }
@@ -48,6 +57,7 @@ const Result: NextPage<ResultProps> = (props: ResultProps) => {
     const food = determineFood();
     const image = determineImage(food);
 
+
     return (
         <div className="w-screen h-screen  bg-gray-800">
             <div className="container mx-auto px-4 py-16">
@@ -58,25 +68,14 @@ const Result: NextPage<ResultProps> = (props: ResultProps) => {
                     <Image priority={true} src={image} alt={food} width={512} height={512}></Image>
                 </div>
             </div>
-
         </div>
     );
-};
+}
 
-export const getStaticProps: GetStaticProps<ResultProps> = async () => {
-    const directory = path.join(process.cwd(), "data");
-    const results = await fs.readFile(path.join(directory, "results.json"), "utf-8");
-    const resultsArray = JSON.parse(results) as Result[];
-    return {
-        props: {
-            results: resultsArray
-        }
-    };
-};
+export async function generateStaticParams() {
+    const paths = Array(16);
+    return paths.map(path => ({params: {result: path}}));
+}
 
-export const getStaticPaths: GetStaticPaths = () => {
-    const paths = [...Array(16).keys()].map((score) => ({params: {result: score.toString()}}));
-    return {paths, fallback: false};
-};
+export default Page;
 
-export default Result;
