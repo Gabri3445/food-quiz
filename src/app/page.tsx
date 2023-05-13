73d29292c5fb5500c2180de2a7519414749beb99
Gabriel Lopez <42178704+Gabri3445@users.noreply.github.com>
2023-05-13 23:24:04 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import path from "path";
 import {promises as fs} from 'fs';
 import Content from "~/components/Content";
 
+export const revalidate = 0;
 
 const Page = async () => {
     const fetchData = async (): Promise<Question[]> => {
